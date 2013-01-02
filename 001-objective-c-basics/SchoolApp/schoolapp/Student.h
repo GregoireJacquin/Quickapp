f2103c684d94af3c21ad0084280d1977a387a90c
gregoirejacquin <gregoire.jacquin@me.com>
2013-01-02 23:24:16 +0100

    Finish

---
+++ 001-objective-c-basics/SchoolApp/schoolapp/Student.h
@@ -0,0 +1,13 @@
+//
+//  Student.h
+//  SchoolApp
+//
+//  Created by Grégoire Jacquin on 02/01/13.
+//  Copyright (c) 2013 NSScreencast. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+
+@end
---
+++ 001-objective-c-basics/SchoolApp/schoolapp/Student.m
@@ -0,0 +1,13 @@
+//
+//  Student.m
+//  SchoolApp
+//
+//  Created by Grégoire Jacquin on 02/01/13.
+//  Copyright (c) 2013 NSScreencast. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+@end
