7646c514169e8ff80b8b78b9dc5877e9fa05193e
gregoirejacquin <gregoire.jacquin@me.com>
2013-01-07 12:42:56 +0100

    Create project on memory problem

---
+++ 002-diagnosing-memory-problems/MemoryProblems/MemoryProblems/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MemoryProblems
+//
+//  Created by Grégoire Jacquin on 07/01/13.
+//  Copyright (c) 2013 Grégoire Jacquin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
