df9962db45e1901af503e1c435fa5a0ab51f99f2
gregoirejacquin <gregoire.jacquin@me.com>
2013-01-07 16:42:16 +0100

    Finish project TableViewBasics

--- 003-tableview-basics/TableViewBasics/TableViewBasics/ViewController.h
+++ 003-tableview-basics/TableViewBasics/TableViewBasics/ViewController.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITableViewDataSource,UITableViewDelegate>
+@property (retain, nonatomic) IBOutlet UITableView *tableView;
+@property (retain, nonatomic) NSArray *beers;
 
 @end
