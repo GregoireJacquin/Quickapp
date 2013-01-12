dfdecfa3447b671b7f174b9a177284893898845d
gregoirejacquin <gregoire.jacquin@me.com>
2013-01-12 21:11:23 +0100

    Clean

---
+++ 005-cocoa-pods/CocoaPodsSample/CocoaPodsSample/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CocoaPodsSample
+//
+//  Created by Grégoire Jacquin on 12/01/13.
+//  Copyright (c) 2013 Grégoire Jacquin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ 005-cocoa-pods/CocoaPodsSample/CocoaPodsSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CocoaPodsSample
+//
+//  Created by Grégoire Jacquin on 12/01/13.
+//  Copyright (c) 2013 Grégoire Jacquin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
