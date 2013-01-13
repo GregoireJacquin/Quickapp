aea1af34da10467a54b0d4c10cebbeba7aa8c628
gregoirejacquin <gregoire.jacquin@me.com>
2013-01-14 00:18:34 +0100

    Create project AFNetworking easy use

---
+++ 006-afnetworking/EasySampleAFNetworking/EasySampleAFNetworking/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  EasySampleAFNetworking
+//
+//  Created by Grégoire Jacquin on 13/01/13.
+//  Copyright (c) 2013 Grégoire Jacquin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ 006-afnetworking/EasySampleAFNetworking/EasySampleAFNetworking/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EasySampleAFNetworking
+//
+//  Created by Grégoire Jacquin on 13/01/13.
+//  Copyright (c) 2013 Grégoire Jacquin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
